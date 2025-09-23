d03bdac2b1fa8ed411d6e7e98565a2daf5e49e7c
Federico <fedee.unates.2001@gmail.com>
2025-09-23 01:04:07 -0300

    Revert "iniciando proyecto"
    This reverts commit 8265a3bf8b9804d69f2d1a575015e41f81a8a6cc.

--- src/config/env.js
+++
@@ -1,6 +0,0 @@
-// src/config/env.js
-import dotenv from "dotenv";
-dotenv.config();
-
-export const PORT = process.env.PORT;
-export const DATABASE_URL = process.env.DATABASE_URL;
--- src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-// src/middlewares/errorHandler.js
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Error interno del servidor" });
-};
-
-export default errorHandler;
